c38de326030534adddeabcba4fe8e0f4d3032627
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-10 21:51:23 -0300

    Function expression with arrow function

--- functions/functionExpression.js
+++ functions/functionExpression.js
@@ -2,4 +2,9 @@ const dobroInFunctionExpression = function (x) {
   return x * 2;
 };
 
+const dobroInFunctionExpressionWithArrowFunction = (x) => {
+  return x * 2;
+};
+
 console.log(dobroInFunctionExpression(5));
+console.log(dobroInFunctionExpressionWithArrowFunction(10));
