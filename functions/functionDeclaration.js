fcf88f41ec327d7aca654404060d4b6b06e2e9c5
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-10 21:47:32 -0300

    First class function expression

--- functions/functionDeclaration.js
+++ functions/functionDeclaration.js
@@ -1,5 +1,5 @@
-function dobro(x) {
+function dobroInFunctionDeclaration(x) {
   return x * 2;
 }
 
-console.log(dobro(2));
+console.log(dobroInFunctionDeclaration(2));
---
+++ functions/functionExpression.js
@@ -0,0 +1,5 @@
+const dobroInFunctionExpression = function (x) {
+  return x * 2;
+};
+
+console.log(dobroInFunctionExpression(5));
